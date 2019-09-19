c7d9d81c938f8a262a8c21437afa2bc347187522
Adolfo Fonzar <adfonzar@gmail.com>
2019-09-19 19:16:57 -0300

    Add Redux Saga

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -25,14 +25,15 @@ class Home extends Component {
     this.setState({ products: data });
   }
 
-  handleAddProduct = product => {
-    const { addToCart } = this.props;
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
 
-    addToCart(product);
+    addToCartRequest(id);
   };
 
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
 
     return (
       <ProductList>
@@ -44,10 +45,11 @@ class Home extends Component {
 
             <button
               type="button"
-              onClick={() => this.handleAddProduct(product)}
+              onClick={() => this.handleAddProduct(product.id)}
             >
               <div>
-                <GiCoffeeBeans size={16} color="#f6f1ec" /> 3
+                <GiCoffeeBeans size={16} color="#f6f1ec" />{' '}
+                {amount[product.id] || 0}
               </div>
 
               <span>COLOCAR NA SACOLA</span>
@@ -59,10 +61,18 @@ class Home extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+
+    return amount;
+  }, {}),
+});
+
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Home);
