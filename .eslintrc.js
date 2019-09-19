616f5c05fadae3d5a6a197ca590633789d0d324d
Adolfo Fonzar <adfonzar@gmail.com>
2019-09-19 16:53:29 -0300

    Configure Reactotron

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
       },
     ],
     'import/prefer-default-export': 'off',
-    'react/state-in-constructor': 0
+    'react/state-in-constructor': 0,
+    'no-console': ["error", { allow: ["tron"] }]
   },
 };
