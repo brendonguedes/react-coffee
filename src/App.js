d7cfbfddfe2c6c9dbca562a1af7ece090779dd38
Adolfo Fonzar <adfonzar@gmail.com>
2019-09-19 09:56:52 -0300

    Add: react-router-dom and Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <p>React Coffe Store</p>
-    </div>
+    <BrowserRouter>
+      {/* <Header/> */}
+      <Routes />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import {  } from './styles';
+
+export default function Home() {
+  return <h1>Home</h1>;
+}
